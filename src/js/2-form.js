71ed52b9080e8d88906be1a5d6538e1793a45e93
Mykhajlo <bmve@ukr.net>
2025-02-27 18:34:52 +0200

    add fix

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,10 +1,10 @@
-import { Input } from "postcss";
-
 const feedbackForm = document.querySelector('.feedback-form');
 const emailInput = feedbackForm.elements.email;
 const messageInput = feedbackForm.elements.message;
 const localStorageKey = "feedback-form-state";
 const maxCount = 500;
+const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const errorMessage = document.getElementById('email-error');
  
 
 let formData = {
@@ -13,28 +13,37 @@ let formData = {
 };
 
 // Парсимо локальне сховище з потрібним нам ключем і витягуєм з нього дані в FormData
-const savedData = JSON.parse(localStorage.getItem(localStorageKey)) || "{}";
+const savedData = JSON.parse(localStorage.getItem(localStorageKey)) || {};
 formData = {
     
-    email: savedData.email,
-    message: savedData.message,
+    email: savedData.email || "",
+    message: savedData.message || "",
 };
 console.log("🚀 ~ formData:", formData);
 
-// заповнюємо дані полів вводу, якщо є збережено то з локального сховища, ні то порожні
-emailInput.value = formData.email || "";
-messageInput.value = formData.message || "";
+// заповнюємо дані полів вводу
+emailInput.value = formData.email;
+messageInput.value = formData.message;
+if (emailInput.value) {
+    validateEmail(formData.email);
+} 
 
 //Додаємо слухача на input і використовуючи делегування вішаєм його на форму
-feedbackForm.addEventListener('input', (event) => {
-    // записуємо значення інпут в formData
-     console.log("🚀 ~ feedbackForm.addEventListener ~  event.target.name:", event.target.name)
-    formData[event.target.name] = event.target.value;
-    console.log("🚀 ~ feedbackForm.addEventListener ~  formData:", formData)
+feedbackForm.addEventListener('input', saveToLocalStr)
+
+function saveToLocalStr(event) {
+    // записуємо значення інпут в formData, перед записом позбуваємося лишніх знаків табуляції(пробіл і ентер)
+    formData[event.target.name] = event.target.value.trim();
+    console.log("🚀 ~ feedbackForm.addEventListener ~  formData:", formData);
+    
+    validateEmail(formData.email);
+
     // записуємо значення formData локальне сховище
-    localStorage.setItem(localStorageKey, JSON.stringify(formData));
+   
+         localStorage.setItem(localStorageKey, JSON.stringify(formData));
     console.log("🚀 ~ feedbackForm.addEventListener ~ localStorage:", localStorage)
-});
+
+};
     
 // Додаємо слухача на кнопку submit, перевірку на пусті поля, якщо все ок: виводимо дані в консоль і все очищуємо
 feedbackForm.addEventListener('submit', (event) => {
@@ -47,5 +56,27 @@ feedbackForm.addEventListener('submit', (event) => {
     formData.email = '';
     formData.message = '';
     feedbackForm.reset();
+    
 })
    
+function validateEmail(email) {
+    const isError = document.querySelector(".error-message")
+    if (!email) {
+         emailInput.classList.remove('error');
+    errorMessage.classList.remove('error-message');
+        errorMessage.textContent = ('');
+        return
+    }
+   if (!regex.test(email)) {
+       console.log("🚀 ~ validateEmail ~ !regex.test(email):", !regex.test(email))
+       emailInput.classList.add('error');
+       errorMessage.classList.add('error-message');
+       errorMessage.textContent = "Некорректний або неповний email! Перевірте правильність вводу!";  
+   } else {
+       emailInput.classList.remove('error');
+       errorMessage.classList.remove('error-message');
+       errorMessage.textContent = ('');
+   }
+
+}
+
